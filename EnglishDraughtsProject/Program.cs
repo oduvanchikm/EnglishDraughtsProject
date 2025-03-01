d32ab23a4ce3dfb53e43095887f90b6672ecff0c
oduvanchikm <mkgubareva2005@gmail.com>
2025-03-01 13:42:19 +0400

    add logger

--- EnglishDraughtsProject/Program.cs
+++ EnglishDraughtsProject/Program.cs
@@ -1,17 +1,40 @@
 ﻿using Avalonia;
 using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace EnglishDraughtsProject;
 
 class Program
 {
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        var services = ConfigureServiceProvider();
 
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+        var addBuilder = BuildAvaloniaApp(services);
+
+        addBuilder.StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp(IServiceProvider serviceProvider) =>
+        AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .AfterSetup(_ =>
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogInformation("Application started.");
+            });
+
+    private static IServiceProvider ConfigureServiceProvider()
+    {
+        var services = new ServiceCollection();
+
+        services.AddLogging(config => { config.AddConsole(); });
+
+
+        return services.BuildServiceProvider();
+    }
 }
